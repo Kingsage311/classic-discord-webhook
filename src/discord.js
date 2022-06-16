f60867eb4470f239b65e128c7b242c65b8009bfa
Kings <84582203+Kingsage311@users.noreply.github.com>
2022-06-16 08:47:25 -0700

    Update discord.js

--- src/discord.js
+++ src/discord.js
@@ -27,7 +27,7 @@ function createEmbed(repo, branch, url, commits, size) {
         .setColor(0x00bb22)
         //.setTitle(size + (size == 1 ? " Commit was " : " Commits were ") + "added to " + repo + " (" + branch + ")")
         .setAuthor({
-            name: `${size} ${size === 1 ? 'commit was ' : 'commits were'} added to  ${repository} ${branch}`,
+            name: `${size} ${size === 1 ? 'commit was ' : 'commits were'} added to  ${repo} ${branch}`,
             iconURL: `https://github.com/${latest.author.username}.png?size=32`,
         })
         .setDescription(`${getChangeLog(commits, size)}`)
